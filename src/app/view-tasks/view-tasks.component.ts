a1cb3e560e97ad6ae07d3e6eeab06ebada7c529f
shohaibMahmud <shohaib.mahmud@gmail.com>
2024-07-30 23:51:17 -0400

    Add rudimentary styling to task view

--- src/app/view-tasks/view-tasks.component.ts
+++ src/app/view-tasks/view-tasks.component.ts
@@ -1,17 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { Task } from '../task.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-view-tasks',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <ul>
+    <ul class="card" [ngClass]="'priority'+task.priority.toString()">
       <li><div class="status"></div>{{task.name}}</li>
       <li>Project: {{task.projName}}</li>
       <li><button class="task-details">Details</button></li>
       <li>Due: {{task.duedate}}</li>
-    </ul>
+</ul>
   `,
   styleUrl: './view-tasks.component.css'
 })
